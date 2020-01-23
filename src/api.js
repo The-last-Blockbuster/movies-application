adf03db28f1f52a1577e9357f2e93e8fc8b6ad5d
Paris Tyus <Tyus.Paris1@gmail.com>
2020-01-23 16:29:28 -0600

    add delete and add movies function

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ module.exports = {
     return fetch('/api/movies')
       .then(response => response.json());
   },
-  createMovie: (title,rating,description,genre ) =>{
+  createMovie: (title,rating,description,genre ) => {
     const MoviePost = {title: title, rating:rating, description:description,genre:genre };
     const url = '/api/movies';
     const options = {
@@ -16,7 +16,57 @@ module.exports = {
     return fetch('/api/movies', options)
         .then(response => response.json())
         .catch(error);
-    console.log(error);
-  }
+    // console.log(error);
+  },
 
+  deleteMovie: (id) => {
+    const deleteMovie = {id:id };
+    const url = '/api/movies/'+id;
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(deleteMovie),
+    };
+    return fetch(url, options)
+        .then(response => response.json())
+        // .catch(error);
+    // console.log(error);
+  },
 };
+
+//
+// };createMovie: (title,rating,description,genre ) =>{
+//     const MoviePost = {title: title, rating:rating, description:description,genre:genre };
+//     const url = '/api/movies';
+//     const options = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(MoviePost),
+//     };
+//     return fetch('/api/movies', options)
+//         .then(response => response.json())
+//         .catch(error);
+//     console.log(error);
+//   }
+//
+// };createMovie: (title,rating,description,genre ) =>{
+//     const MoviePost = {title: title, rating:rating, description:description,genre:genre };
+//     const url = '/api/movies';
+//     const options = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(MoviePost),
+//     };
+//     return fetch('/api/movies', options)
+//         .then(response => response.json())
+//         .catch(error);
+//     console.log(error);
+//   }
+//
+// };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 /**
  * es6 modules and imports
  */
+
+
 const $ = require("jquery");
 
 import sayHello from './hello';
@@ -9,10 +11,21 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies, createMovie} = require('./api.js');
+
+//functionality for the loading pane//
+$(document).ready(function () {
+  $("#extraControls").hide(8000).show();
+  $('#products').hide().show();
+  $('#refresh').hide().show();
+});
+
+let loader1 = $('.container').hide().show(3000).hide(3000);
+let loader2 = $('.center').hide().show(3000).hide(3000);
+
+const {getMovies, createMovie, deleteMovie} = require('./api.js');
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
-  $('.container').hide();
+  // $('#products').hide().show();
 
   movies.forEach(({title, rating, description, genre, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating} - ${description} - ${genre}`);
@@ -38,24 +51,18 @@ getMovies().then((movies) => {
       console.log(select);
       createMovie(movieTitle,rating,movieDescription,select);
     });
-    //     $().html("you created a new movie");
-  //     .html("you added a new movie")
 
-      // $('.add-movies').append('/db.json')
+//delete movie//
 
-      // ('<tr><td>' + `id#${id}` + '</td><td>' + ' ' + `${title}` + '</td><td>' + `${rating}` + '</td><td>' + `${description}` + '</td><td>' + `${genre}` + '</td></tr>')
-  //   })
-  // });
-
-
-
-//functionality for the loading pane//
+  $('#delete').click(function () {
+    let deletion = $('#delete1').val();
+    console.log(deletion);
+    deleteMovie(deletion);
+  });
 
 
-  $(document).ready(function () {
-    $('#products').hide().show();
-    $('#refresh').hide().show();
-    let loader1 = $('.container').hide().show(3000).hide(3000);
-    let loader2 = $('.center').hide().show(3000).hide(3000);
-   });
-// },);
+
+
+
+
+
