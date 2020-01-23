495009ffbbcdda15ddd907852a8749207e7a2921
Gaston Tarango <gaston.tarango@gmail.com>
2020-01-23 10:38:51 -0600

    .

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating} - ${genre}`);
+  movies.forEach(({title, rating, description, genre, id}) => {
+    console.log(`id#${id} - ${title} - rating: ${rating} - ${description} - ${genre}`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
@@ -23,7 +23,7 @@ function refresh() {
   let inventory = $.get("data/inventory.json");
   inventory.done(function (data) {
     $.each(data, function (index, item) {
-      $('#insertProducts').append('<tr>' + '<td>' + item.title + '</td>' + '<td>' + item.quantity + '</td>' + '<td>' + item.price + '</td>' + '<td>' + item.categories + '</td>' + '</tr>');
+      $('#insertProducts').append('<tr>' + '<td>' + item.title + '</td>' + '<td>' + item.rating + '</td>' + '<td>' + item.description + '</td>' + '<td>' + item.genre + '</td>' + '</tr>');
     })
   })
 }
@@ -32,3 +32,9 @@ $('#refresh').click(function () {
   $('#insertProducts').html("");
   refresh()
 })
+//functionality for the loading pane//
+
+$(document).ready(function () {
+  let loader1 = $('.container').hide().show(8000).hide(8000);
+  let loader2 = $('.center').hide().show(8000).hide(8000);
+});
