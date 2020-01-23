4c27cc767c4a6cc6fc256a73ee45ccaecbcf3de0
Paris Tyus <Tyus.Paris1@gmail.com>
2020-01-23 15:28:42 -0600

    alot of work so far

--- src/api.js
+++ src/api.js
@@ -2,5 +2,21 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+  createMovie: (title,rating,description,genre ) =>{
+    const MoviePost = {title: title, rating:rating, description:description,genre:genre };
+    const url = '/api/movies';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(MoviePost),
+    };
+    return fetch('/api/movies', options)
+        .then(response => response.json())
+        .catch(error);
+    console.log(error);
   }
+
 };
--- src/index.js
+++ src/index.js
@@ -2,41 +2,60 @@
  * es6 modules and imports
  */
 const $ = require("jquery");
+
 import sayHello from './hello';
 sayHello('World');
 
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
-
+const {getMovies, createMovie} = require('./api.js');
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
+  $('.container').hide();
+
   movies.forEach(({title, rating, description, genre, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating} - ${description} - ${genre}`);
     $('#products').append('<tr><td>' +`id#${id}` + '</td><td>' + ' ' + `${title}` + '</td><td>' + `${rating}` + '</td><td>' + `${description}` + '</td><td>' + `${genre}` + '</td></tr>')
   });
+
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.Check the console for details.');
   console.log(error);
+
 });
 
-function refresh() {
-  let inventory = $.get("./db.json");
-  inventory.done(function (data) {
-    $.each(data, function (index, item) {
-      $('#insertProducts').append('<tr>' + '<td>' + item.title + '</td>' + '<td>' + item.rating + '</td>' + '<td>' + item.description + '</td>' + '<td>' + item.genre + '</td>' + '</tr>');
-    })
-  })
-}
-refresh();
-$('#refresh').click(function () {
-  $('#insertProducts').html("");
-  refresh()
-})
+//add movie functioning//
+
+    $('#refresh').click(function () {
+      let movieTitle = $('#movie-title').val();
+      console.log(movieTitle);
+      let rating = $('#movie-rating').val();
+      console.log(rating);
+      let movieDescription = $('#movie-description').val();
+      console.log(movieDescription);
+      let select = $('#select').val();
+      console.log(select);
+      createMovie(movieTitle,rating,movieDescription,select);
+    });
+    //     $().html("you created a new movie");
+  //     .html("you added a new movie")
+
+      // $('.add-movies').append('/db.json')
+
+      // ('<tr><td>' + `id#${id}` + '</td><td>' + ' ' + `${title}` + '</td><td>' + `${rating}` + '</td><td>' + `${description}` + '</td><td>' + `${genre}` + '</td></tr>')
+  //   })
+  // });
+
+
+
 //functionality for the loading pane//
 
-$(document).ready(function () {
-  let loader1 = $('.container').hide().show(8000).hide(8000);
-  let loader2 = $('.center').hide().show(8000).hide(8000);
-});
+
+  $(document).ready(function () {
+    $('#products').hide().show();
+    $('#refresh').hide().show();
+    let loader1 = $('.container').hide().show(3000).hide(3000);
+    let loader2 = $('.center').hide().show(3000).hide(3000);
+   });
+// },);
